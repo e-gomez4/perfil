ee5cf96b8499bd7c0db80dd58f4d2ce2aadf0429
e-gomez4 <124343051+e-gomez4@users.noreply.github.com>
2023-03-18 16:24:43 +0000

    a

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -1,2 +1,9 @@
-
-src="javascript/funciones.js"
+function mostrarDivPrincipal() {
+    const boton = document.getElementById("botonIniciar");
+    const divPrincipal = document.getElementById("contenedorPrincipal");
+    
+    boton.addEventListener("click", function() {
+      boton.style.display = "none"; // oculta el botón
+      divPrincipal.style.display = "block"; // muestra el div principal
+    });
+}
