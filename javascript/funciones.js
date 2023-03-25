4a41129d144ad50d29313c5dcb3c992d4549d2df
e-gomez4 <124343051+e-gomez4@users.noreply.github.com>
2023-03-25 14:49:06 +0000

    a

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -32,5 +32,26 @@ document.addEventListener("DOMContentLoaded", function() {
     botonIniciar.style.display = "none"; // oculta el botón
     divPrincipal.style.display = "block"; // muestra el div principal
   });
+
+  const bodyPrincipal = document.getElementById("principal");
+  const botondarkmode = document.getElementById("darkmode");
+  const divDespuesHeader = document.getElementById("despuesHeader");
+  const divSegundoHeader = document.getElementById("segundoHeader");
+  const divtercerHeader = document.getElementById("tercerHeader");
+  
+  botondarkmode.addEventListener("click", function() {
+    var letralista = document.getElementsByClassName("letra-divs");
+    for (var i = 0; i < letralista.length; i++) {
+      letralista[i].style.color = "white";
+    }
+    bodyPrincipal.style.backgroundImage = "url(../imagenes/body-background3.jpg)"
+    divDespuesHeader.style.backgroundColor = "#1a1a1a";
+    divSegundoHeader.style.backgroundColor = "#1a1a1a"; 
+    divtercerHeader.style.backgroundColor = "#1a1a1a";
+    var titulosdivs = document.getElementsByClassName("titulos-divs");
+    for (var i = 0; i < titulosdivs.length; i++) {
+      titulosdivs[i].style.color = "white";
+    }
+  });
 });
 
