e4b4f196e17ce4222a2f249b47f6af6cbc7047c3
e-gomez4 <124343051+e-gomez4@users.noreply.github.com>
2023-03-25 17:51:23 +0000

    a

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -34,6 +34,7 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   const bodyPrincipal = document.getElementById("principal");
+  const headerTop = document.getElementById("top");
   const botondarkmode = document.getElementById("darkmode");
   const divDespuesHeader = document.getElementById("despuesHeader");
   const divSegundoHeader = document.getElementById("segundoHeader");
@@ -49,9 +50,14 @@ document.addEventListener("DOMContentLoaded", function() {
       }
       bodyPrincipal.style.backgroundImage = "none";
       bodyPrincipal.style.backgroundColor = "#e7e7e7";
+      //headerTop.style.borderBottomColor = "#a4a5a594";
       divDespuesHeader.style.backgroundColor = "#c2c2c2";
+      divDespuesHeader.style.borderColor = "#a4a5a594";
       divSegundoHeader.style.backgroundColor = "#c2c2c2"; 
+      divSegundoHeader.style.borderColor = "#a4a5a594";
       divtercerHeader.style.backgroundColor = "#c2c2c2";
+      divtercerHeader.style.borderColor = "#a4a5a594";
+      botondarkmode.style.backgroundColor = "khaki"
       var titulosdivs = document.getElementsByClassName("titulos-divs");
       for (var i = 0; i < titulosdivs.length; i++) {
         titulosdivs[i].style.color = "black";
@@ -63,10 +69,17 @@ document.addEventListener("DOMContentLoaded", function() {
       for (var i = 0; i < letralista2.length; i++) {
         letralista2[i].style.color = "white";
       }
-      bodyPrincipal.style.backgroundImage = "url(../imagenes/body-background3.jpg)"
+      bodyPrincipal.style.backgroundImage = "linear-gradient(rgba(20, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(../imagenes/body-background3.jpg)";
+      divDespuesHeader.style.transition = "1s ease"
       divDespuesHeader.style.backgroundColor = "#1a1a1a";
-      divSegundoHeader.style.backgroundColor = "#1a1a1a"; 
+      divDespuesHeader.style.borderColor = "#429ba7";
+      divSegundoHeader.style.transition = "1s ease"
+      divSegundoHeader.style.backgroundColor = "#1a1a1a";
+      divSegundoHeader.style.borderColor = "#429ba7";
+      divtercerHeader.style.transition = "1s ease"
       divtercerHeader.style.backgroundColor = "#1a1a1a";
+      divtercerHeader.style.borderColor = "#429ba7";
+      botondarkmode.style.backgroundColor = "gray"
       var titulosdivs = document.getElementsByClassName("titulos-divs");
       for (var i = 0; i < titulosdivs.length; i++) {
         titulosdivs[i].style.color = "white";
