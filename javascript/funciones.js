153bf8042e0a6700179b99c752d91541b0a81a79
e-gomez4 <124343051+e-gomez4@users.noreply.github.com>
2023-03-27 20:39:40 +0000

    a

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -58,6 +58,14 @@ document.addEventListener("DOMContentLoaded", function() {
       divtercerHeader.style.backgroundColor = "#c2c2c2";
       divtercerHeader.style.borderColor = "#a4a5a594";
       botondarkmode.style.backgroundColor = "khaki"
+      var titulosDesplegables = document.getElementsByClassName("titulo-desplegable");
+      for (var i = 0; i < titulosDesplegables.length; i++) {
+        titulosDesplegables[i].style.color = "black";
+      }
+      var imgDesplegables = document.getElementsByClassName("flecha-abajo");
+      for (var i = 0; i < imgDesplegables.length; i++) {
+        imgDesplegables[i].style.filter = "invert(0%)";
+      }
       var titulosdivs = document.getElementsByClassName("titulos-divs");
       for (var i = 0; i < titulosdivs.length; i++) {
         titulosdivs[i].style.color = "black";
@@ -69,7 +77,8 @@ document.addEventListener("DOMContentLoaded", function() {
       for (var i = 0; i < letralista2.length; i++) {
         letralista2[i].style.color = "white";
       }
-      bodyPrincipal.style.backgroundImage = "linear-gradient(rgba(20, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(../imagenes/body-background3.jpg)";
+      /*bodyPrincipal.style.backgroundImage = "linear-gradient(rgba(20, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(../imagenes/body-background3.jpg)";*/
+      bodyPrincipal.style.backgroundImage = "url(../imagenes/body-background3.jpg)";
       divDespuesHeader.style.transition = "1s ease"
       divDespuesHeader.style.backgroundColor = "#1a1a1a";
       divDespuesHeader.style.borderColor = "#429ba7";
@@ -79,13 +88,47 @@ document.addEventListener("DOMContentLoaded", function() {
       divtercerHeader.style.transition = "1s ease"
       divtercerHeader.style.backgroundColor = "#1a1a1a";
       divtercerHeader.style.borderColor = "#429ba7";
-      botondarkmode.style.backgroundColor = "gray"
+      botondarkmode.style.backgroundColor = "gray";
+      var titulosDesplegables = document.getElementsByClassName("titulo-desplegable");
+      for (var i = 0; i < titulosDesplegables.length; i++) {
+        titulosDesplegables[i].style.color = "white";
+      }
+      var imgDesplegables = document.getElementsByClassName("flecha-abajo");
+      for (var i = 0; i < imgDesplegables.length; i++) {
+        imgDesplegables[i].style.filter = "invert(100%)";
+      }
       var titulosdivs = document.getElementsByClassName("titulos-divs");
       for (var i = 0; i < titulosdivs.length; i++) {
         titulosdivs[i].style.color = "white";
       }
       modoOscuro = true;
     }
-  });  
+  }); 
+  const divDesplegable1 = document.getElementById("desplegable1");
+  const divDesplegableOculto1 = document.getElementById("desplegable-oculto1");
+  const ImgDesplegable1 = document.getElementById("imagen-desplegable1");
+  const divDesplegable2 = document.getElementById("desplegable2");
+  const divDesplegableOculto2 = document.getElementById("desplegable-oculto2");
+  const ImgDesplegable2 = document.getElementById("imagen-desplegable2");
+
+  divDesplegable1.addEventListener("click", function() {
+    if (divDesplegableOculto1.style.display === "none") {
+      divDesplegableOculto1.style.display = "flex";
+      ImgDesplegable1.style.transform = "rotate(180deg)";
+    } else {
+      divDesplegableOculto1.style.display = "none";
+      ImgDesplegable1.style.transform = "rotate(0deg)";
+    }
+  });
+
+  divDesplegable2.addEventListener("click", function() {
+    if (divDesplegableOculto2.style.display === "none") {
+      divDesplegableOculto2.style.display = "flex";
+      ImgDesplegable2.style.transform = "rotate(180deg)";
+    } else {
+      divDesplegableOculto2.style.display = "none";
+      ImgDesplegable2.style.transform = "rotate(0deg)";
+    }
+  });
 });
 
